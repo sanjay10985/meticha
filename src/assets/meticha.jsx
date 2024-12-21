1776e9521832c028d0748450887d348c1f09445e
sanjay10985 <sanjaytomar717@gmail.com>
2024-12-21 19:36:21 +0530

    changed image

--- src/assets/meticha.jsx
+++ src/assets/meticha.jsx
@@ -3,7 +3,11 @@ import { withPhysics } from "../components/with-physics";
 
 const Meticha = (props) => {
   return (
-    <img className="cursor-none" src="/public/Meticha_Logo.png" {...props} />
+    <img
+      className="cursor-none"
+      src="/src/assets/Meticha_Logo.png"
+      {...props}
+    />
   );
 };
 
